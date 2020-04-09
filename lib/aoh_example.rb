759a01eb095cad3b45a0da41994c6fa84860c4d6
Jessica K <jessikim807@gmail.com>
2020-04-09 01:43:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,5 +1,6 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-require 'pry' 
+require 'pry'
+
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
@@ -29,7 +30,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] = "new_value"
+  aoh_update = (aoh[row][key] = "new_value")
   binding.pry
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
