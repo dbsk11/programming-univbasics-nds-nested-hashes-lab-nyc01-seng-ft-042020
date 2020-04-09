858d76f567300648500b815aa80fa1cfd7bfc2e8
Jessica K <jessikim807@gmail.com>
2020-04-09 01:44:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,7 +31,6 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh_update = (aoh[row][key] = new_value)
-  binding.pry
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
